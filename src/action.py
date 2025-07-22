8c1aebb4f89470af9c3106f78b2d9a29c887b7fa
Héctor S. N. <hecsaen@users.noreply.github.com>
2025-07-22 11:09:52 +0200

    release: first working version (#1)
    * Revert "fix..."
    
    This reverts commit aceb93e16f09600ab966524a8d94562382f4ba3f.
    
    * Revert "fix: push Dockerfile"
    
    This reverts commit 35cabe2a18dfbf437b010f8836822ddf57b2ae9b.
    
    * Revert "feat: change to dockerfile"
    
    This reverts commit 96903d93a5ed51e22cf60f7b41fbd9cdd0bb156e.
    
    * feat: emmbed python
    
    * fix: add python shell
    
    * fix: typo

--- src/action.py
+++ src/action.py
@@ -1,34 +1,28 @@
-# %%
 import json
 import os
 import requests
 
-print("====== PRINTING ALL ENV VARS ======")
-print(os.environ)
-print("===================================")
-
-ENV_SECRET = os.getenv("INPUT_SECRET_ENVIRONMENT")
-ENV_PUBLIC = os.getenv("INPUT_PUBLIC_ENVIRONMENT")
-ENV_TARGET = os.getenv("INPUT_TARGET_ENVIRONMENT")
+ENV_SECRET = os.getenv("POSTMAN_ENV_SECRET")
+ENV_PUBLIC = os.getenv("POSTMAN_ENV_PUBLIC")
+ENV_TARGET = os.getenv("POSTMAN_ENV_TARGET")
 
 API_KEY = os.getenv("POSTMAN_APIKEY")
 HEADERS={"X-API-Key": API_KEY}
 
+print(os.en)
 
-
-# %%
 def get_environment(environment_id):
-    response = requests.get(f"https://api.getpostman.com/environments/{environment_id}d", headers=HEADERS)
+    response = requests.get(f"https://api.getpostman.com/environments/{environment_id}", headers=HEADERS)
     if response.status_code != 200:
-        raise Exception(f"Environment {environment_id} could not be retrieved: {json.dumps(response.json(), indent=2)}")
+        message = f"Environment {environment_id} could not be retrieved: {json.dumps(response.json(), indent=2)}"
+        print(message)
+        raise Exception(message)
     return response.json()
     
 
 env_public = get_environment(ENV_PUBLIC)
 env_secret = get_environment(ENV_SECRET)
 env_target = get_environment(ENV_TARGET)
-
-# %%
 
 def merge_values(origin_values, secret_values):
     secrets_dict = dict([(x["key"], x) for x in secret_values])
@@ -46,16 +40,9 @@ def merge_values(origin_values, secret_values):
 merged_values = merge_values(env_public["environment"]["values"], env_secret["environment"]["values"])
 
 env_target["environment"]["values"] = merged_values
-
-# %%
-# print(json.dumps(env_target, indent=2))
-
-# %%
-
 response = requests.put(f"https://api.getpostman.com/environments/{ENV_TARGET}", data=json.dumps(env_target), headers=HEADERS)
 
 if 200 == response.status_code:
     print(f"Environment '{env_target['environment']['name']}' updated successfully.")
 else:
-    raise Exception(f"Environment was not updated: {json.dumps(response.json(), indent=2)}")
-# %%
+    raise Exception(f"Environment '{ENV_TARGET} was not updated: {json.dumps(response.json(), indent=2)}")
